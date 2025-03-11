2b500e0e7aadc1d2c59778284f5cefd0c0c08c9c
jennyo <jennifer.oleary.10@gmail.com>
2025-03-11 16:25:30 -0500

    clean up

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 /*
 03/11/25
 practice-character-checker-jennifer-o
+Assignment: Practice: Stringing Characters Together - 1: Character Checker
 
-Instructions: Create a small program that prompts the user for a word or phrase and then allows
+Instructions: 
+Create a small program that prompts the user for a word or phrase and then allows
 them to query specific positions within that string.
-Your application should:
+
 1. Use readlineSync.question() to prompt a user for input
 2. Prompt a user for an index number to find the character at that index
 3. Use bracket notation to access the character
