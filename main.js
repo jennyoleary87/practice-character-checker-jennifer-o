d65e583635e5e05ba72c70875d9e6b281018f7ee
jennyo <jennifer.oleary.10@gmail.com>
2025-03-11 16:07:48 -0500

    instructions comment

---
+++ main.js
@@ -0,0 +1,13 @@
+/*
+03/11/25
+practice-character-checker-jennifer-o
+
+Instructions: Create a small program that prompts the user for a word or phrase and then allows
+them to query specific positions within that string.
+Your application should:
+1. Use readlineSync.question() to prompt a user for input
+2. Prompt a user for an index number to find the character at that index
+3. Use bracket notation to access the character
+4. Print out the character
+*/
+
